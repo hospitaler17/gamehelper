98df80effff3cbee590a8528d55129f21dc12273
NikolasBolas <134624929+NikolasBolas@users.noreply.github.com>
2023-05-26 18:38:43 +0300

    Added some attributes for Spell. Expanded common.h.

--- objects/spell.h
+++ objects/spell.h
@@ -5,12 +5,24 @@
 #include <QDateTime>
 
 #include "objects/baseobject.h"
+#include "common.h"
 
 class Spell : public BaseObject
 {
     Q_OBJECT
 public:
+
+    enum SPELLTYPE
+    {
+        ST_DAMAGE = 0,
+        ST_HEAL,
+        ST_BUFF,
+        ST_DEBUFF,
+     };
+
     explicit Spell(BaseObject *parent = 0);
+
+
 
     QString description() const;
     void setDescription(const QString &description);
@@ -20,8 +32,21 @@ public:
 
 protected:
 
-    QString _description;
-    quint8  _cooldawn;
+    QString _description;   //!> Описание скила
+    quint8  _cooldawn;      //!> Откат скила, количество ходов до доступности
+    EFFECT_TYPE _effectType = ET_NONE; //!> Эффект, накладываемый заклинанием, определяется для ST_BUFF & ST_DEBUFF
+    quint8 _value =0; //!> Значение урона или лечение наносимое скилом
+    quint8 _range =1; //!> Дальность использования навыка
+    quint8 _castRadius=1;   //!> Дальность распространения навыка, относително точки применения
+
+    QHash<QPair<quint8, quint8>,quint8> _area; //!> Область применения скила
+    DIRECTIONSIDE _direction; //!> Направления применения навыка
+
+
+
+
+
+
 signals:
 
 public slots:
